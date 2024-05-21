f7a5389c6ed1b44188c7764587c265f96ea37656
sara <sarapajarin001@gmail.com>
2024-05-21 11:04:48 +0200

    primer commit, tenedor

---
+++ printCubierto.js
@@ -0,0 +1,14 @@
+Console.log("l l l");
+Console.log("l l l");
+Console.log("l l l");
+Console.log("l l l");
+Console.log("l_l_l");
+Console.log("l l l");
+Console.log("l l l");
+Console.log("l l l");
+Console.log("l l l");
+Console.log("l l l");
+Console.log("l l l");
+Console.log("l l l");
+Console.log("l l l");
+
